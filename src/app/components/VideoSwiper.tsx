7e7501ccd730bee4060eed6f4642fc559058c940
andresCamp <andreslcampos@icloud.com>
2023-07-28 07:35:52 -0600

    full draft r15

--- src/app/components/VideoSwiper.tsx
+++ src/app/components/VideoSwiper.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { ReactNode, useEffect, useRef } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 interface VideoSwiperProps {
   children: ReactNode;
