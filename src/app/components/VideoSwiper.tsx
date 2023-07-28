593e8bf87bce06ac50020c80a92d6ac03b4d8dfe
andresCamp <andreslcampos@icloud.com>
2023-07-28 06:26:37 -0600

    full draft r5

--- src/app/components/VideoSwiper.tsx
+++ src/app/components/VideoSwiper.tsx
@@ -1,8 +1,11 @@
 'use client'
 
-import { ReactNode } from 'react';
+import { ReactNode, use } from 'react';
 import { useRouter } from 'next/navigation';
-import { useSwipeable } from 'react-swipeable';
+// import { useSwipeable } from 'react-swipeable';
+import  { useGesture}  from 'react-use-gesture';
+
+// Other imports...
 
 interface VideoSwiperProps {
   children: ReactNode;
@@ -12,22 +15,52 @@ interface VideoSwiperProps {
 const VideoSwiper: React.FC<VideoSwiperProps> = ({ children, index }) => {
   const router = useRouter();
 
-  const handlers = useSwipeable({
-    onSwipedUp: () => {
-      router.push(`/library/video/short${index + 1}`);
-    },
-    onSwipedDown: () => {
-      if (index > 0) {
-        router.push(`/library/video/short${index - 1}`);
+  const bind = useGesture({
+    onDrag: ({ swipe }) => {
+        if (swipe[1] > 0) { // Swipe Up
+          router.push(`/library/video/short${index + 1}`);
+        } else if (swipe[1] < 0 && index > 0) { // Swipe Down
+          router.push(`/library/video/short${index - 1}`);
+        }
       }
-    },
-    trackMouse: true
-  });
+    });
 
-  return <div {...handlers}>{children}</div>;
+  return <div {...bind()}>{children}</div>;
 };
 
+
 export default VideoSwiper;
+
+// 'use client'
+
+// import { ReactNode } from 'react';
+// import { useRouter } from 'next/navigation';
+// import { useSwipeable } from 'react-swipeable';
+
+// interface VideoSwiperProps {
+//   children: ReactNode;
+//   index: number;
+// }
+
+// const VideoSwiper: React.FC<VideoSwiperProps> = ({ children, index }) => {
+//   const router = useRouter();
+
+//   const handlers = useSwipeable({
+//     onSwipedUp: () => {
+//       router.push(`/library/video/short${index + 1}`);
+//     },
+//     onSwipedDown: () => {
+//       if (index > 0) {
+//         router.push(`/library/video/short${index - 1}`);
+//       }
+//     },
+//     trackMouse: true
+//   });
+
+//   return <div {...handlers}>{children}</div>;
+// };
+
+// export default VideoSwiper;
 
 
 // import { ReactNode } from 'react';
--- src/app/components/YouTubePlayer.tsx
+++ src/app/components/YouTubePlayer.tsx
@@ -13,7 +13,7 @@ const YouTubePlayer: React.FC<YouTubePlayerProps> = ({ youtubeLink }) => {
         src={youtubeLink}
         title="YouTube video player"
         frameBorder="0"
-        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
     ></iframe>
   );
