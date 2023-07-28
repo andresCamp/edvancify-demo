0a1539d4e7d6e287b1a87ce32d26dec0fd87b165
andresCamp <andreslcampos@icloud.com>
2023-07-28 06:06:13 -0600

    full draft r3

--- src/app/components/ShortsContainerPage.tsx
+++ src/app/components/ShortsContainerPage.tsx
@@ -1,61 +1,61 @@
-import { useRouter } from 'next/router';
-import { useSwipeable } from 'react-swipeable';
-import { shortsData } from '../library/video/data';
-import ShortPage from '../library/video/page';
+// import { useRouter } from 'next/router';
+// import { useSwipeable } from 'react-swipeable';
+// import { shortsData } from '../library/video/data';
+// import ShortPage from '../library/video/page';
 
-interface Short {
-  id: string;
-  youtubeLink: string;
-  questions: Array<{
-    questionText: string;
-    correctAnswer: string;
-    options: string[];
-  }>;
-}
+// interface Short {
+//   id: string;
+//   youtubeLink: string;
+//   questions: Array<{
+//     questionText: string;
+//     correctAnswer: string;
+//     options: string[];
+//   }>;
+// }
 
-const ShortsContainerPage = () => {
-  const router = useRouter();
-  const { shortId } = router.query;
-  const handlers = useSwipeable({
-    onSwipedUp: () => {
-      const nextId = nextShortId(shortId as string);
-      router.push(`/${nextId}`);
-    },
-    onSwipedDown: () => {
-      const previousId = previousShortId(shortId as string);
-      router.push(`/${previousId}`);
-    },
+// const ShortsContainerPage = () => {
+//   const router = useRouter();
+//   const { shortId } = router.query;
+//   const handlers = useSwipeable({
+//     onSwipedUp: () => {
+//       const nextId = nextShortId(shortId as string);
+//       router.push(`/${nextId}`);
+//     },
+//     onSwipedDown: () => {
+//       const previousId = previousShortId(shortId as string);
+//       router.push(`/${previousId}`);
+//     },
     
-    trackMouse: true,
-  });
+//     trackMouse: true,
+//   });
 
-  const nextShortId = (currentId: string): string => {
-    const currentIndex = shortsData.findIndex(short => short.id === currentId);
-    if (currentIndex === -1 || currentIndex === shortsData.length - 1) {
-      // return the first id if the current one is not found or it's the last one
-      return shortsData[0].id;
-    } else {
-      // return the next id
-      return shortsData[currentIndex + 1].id;
-    }
-  };
+//   const nextShortId = (currentId: string): string => {
+//     const currentIndex = shortsData.findIndex(short => short.id === currentId);
+//     if (currentIndex === -1 || currentIndex === shortsData.length - 1) {
+//       // return the first id if the current one is not found or it's the last one
+//       return shortsData[0].id;
+//     } else {
+//       // return the next id
+//       return shortsData[currentIndex + 1].id;
+//     }
+//   };
   
-  const previousShortId = (currentId: string): string => {
-    const currentIndex = shortsData.findIndex(short => short.id === currentId);
-    if (currentIndex === -1 || currentIndex === 0) {
-      // return the last id if the current one is not found or it's the first one
-      return shortsData[shortsData.length - 1].id;
-    } else {
-      // return the previous id
-      return shortsData[currentIndex - 1].id;
-    }
-  };
+//   const previousShortId = (currentId: string): string => {
+//     const currentIndex = shortsData.findIndex(short => short.id === currentId);
+//     if (currentIndex === -1 || currentIndex === 0) {
+//       // return the last id if the current one is not found or it's the first one
+//       return shortsData[shortsData.length - 1].id;
+//     } else {
+//       // return the previous id
+//       return shortsData[currentIndex - 1].id;
+//     }
+//   };
   
-  return (
-    <div {...handlers}>
-      <ShortPage />
-    </div>
-  );
-};
+//   return (
+//     <div {...handlers}>
+//       <ShortPage />
+//     </div>
+//   );
+// };
 
-export default ShortsContainerPage;
+// export default ShortsContainerPage;
